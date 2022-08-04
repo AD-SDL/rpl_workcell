07340fbcfbf08ed3c1115e298fd76c7f76f9c2af
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-04 13:54:54 -0500

    Seperating demo workcell from pcr, cleaning structure a bit

--- demo-workcell/wc_client.py
+++ demo-workcell/wc_client.py
@@ -1,15 +1,24 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+# TODO: Currently does not work due to relative imports,
+# when this becomes a package it will be good
+from ..maestro.wc_client_base import WC_Client
 
 
-
-from rpl_maestro import WC_Client
-
-rpl_workcell_path = ''
-
-client = WC_Client(rpl_workcell_path)
-
-client.check_modules()
-
-client.check_actions()
+def main(args):
+    wc = WC_Client(args.workflow)
+    if args.verbose:
+        wc.print_flow()
+        wc.print_workcell()
+    wc.check_modules()
+    wc.check_actions()
 
 
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-wf", "--workflow", help="Path to workflow file", type=Path, required=True)
+    parser.add_argument("-v", "--verbose", help="Extended printing options", action="store_true")
 
+    args = parser.parse_args()
+    main(args)
