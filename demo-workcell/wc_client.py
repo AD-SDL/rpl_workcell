8f5f4b8b795c287620b32a5503750b847cf835fb
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-16 14:06:08 -0500

    Updating example client to follow new API

--- demo-workcell/wc_client.py
+++ demo-workcell/wc_client.py
@@ -3,16 +3,16 @@ from argparse import ArgumentParser
 
 # TODO: Currently does not work due to relative imports,
 # when this becomes a package it will be good
-from ..maestro.wc_client_base import WC_Client
+from rpl_wei.wei_client_base import WEI
 
 
 def main(args):
-    wc = WC_Client(args.workflow)
+    wei = WEI(args.workflow)
     if args.verbose:
-        wc.print_flow()
-        wc.print_workcell()
-    wc.check_modules()
-    wc.check_actions()
+        wei.print_flow()
+        wei.print_workcell()
+    wei.check_modules()
+    wei.check_flowdef()
 
 
 if __name__ == "__main__":
