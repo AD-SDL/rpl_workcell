b980547f3bca27edcfb16b6c4497f81b8447fb34
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-08-26 10:28:56 -0500

    new weekly example

--- pcr_workcell/wc_client.py
+++ pcr_workcell/wc_client.py
@@ -4,6 +4,20 @@ from argparse import ArgumentParser
 from rpl_wei.wei_client_base import WEI
 import logging
 
+def print_callback(step):
+    print(step)
+
+##
+from wei_executor.weiExecutorNode import weiExecNode
+def wei_service_callback(step):
+    wei_execution_node = weiExecNode()
+    rclpy.init() # without this line the weiExecNode does not get started
+
+    #get all info
+    #get info from workcell
+    #substitute things from wc_dictionary
+    #send_wei_command(service,action_handler,actions_vars)
+##
 
 def main(args):
     wei = WEI(
@@ -16,7 +30,7 @@ def main(args):
     # get the workflow id (currently defaulting to first one available)
     wf_id = list(wei.get_workflows().keys())[0]
 
-    wei.run_workflow(wf_id)
+    wei.run_workflow(wf_id, step_execution_callback=wei_service_callback)
 
 
 if __name__ == "__main__":
