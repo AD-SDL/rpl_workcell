dc2a47f4a1d89baaa33388af9da16afb5eee25d2
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-22 11:27:21 -0500

    Update install.sh

--- pcr_workcell/install.sh
+++ pcr_workcell/install.sh
@@ -5,9 +5,11 @@ ROS_WS=~/wei_ws_week3
 mkdir -p $ROS_WS
 mkdir -p $ROS_WS/src
 cd $ROS_WS/src
-git clone https://github.com/AD-SDL/azenta_driver
-git clone https://github.com/AD-SDL/hudson_driver
-git clone https://github.com/AD-SDL/pf400_driver
+git clone https://github.com/AD-SDL/azenta_module
+git clone https://github.com/AD-SDL/hudson_module
+git clone https://github.com/AD-SDL/pf400_module
+git clone https://github.com/AD-SDL/ot2_driver
+git clone https://github.com/AD-SDl/ot2_driver_ros # to be merged with driver later
 git clone https://github.com/AD-SDL/wei_ros
 git clone https://github.com/AD-SDL/rpl_wei
 cd $ROS_WS/src/rpl_wei
@@ -18,4 +20,3 @@ git clone https://github.com/AD-SDL/rpl_workcell
 cd $ROS_WS
 colcon build
 
-#TODO: OT2 docker image thing
