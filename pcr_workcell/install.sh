67f13e3593ecfae542884f7dbbae47d58cb568a9
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-28 15:53:14 -0500

    install fixes

--- pcr_workcell/install.sh
+++ pcr_workcell/install.sh
@@ -8,15 +8,16 @@ cd $ROS_WS/src
 git clone https://github.com/AD-SDL/azenta_module
 git clone https://github.com/AD-SDL/hudson_module
 git clone https://github.com/AD-SDL/pf400_module
-git clone https://github.com/AD-SDL/ot2_driver
-git clone https://github.com/AD-SDl/ot2_driver_ros # to be merged with driver later
+git clone --recurse-submodules https://github.com/AD-SDl/ot2_module 
 git clone https://github.com/AD-SDL/wei_ros
+cd ..
+rosdep install --from-paths src --ignore-src
+colcon build
+
+
 git clone https://github.com/AD-SDL/rpl_wei
 cd $ROS_WS/src/rpl_wei
 pip3 install -r requirements/requirements.txt
 pip3 install -e .
-cd $ROS_WS/src
-git clone https://github.com/AD-SDL/rpl_workcell
 cd $ROS_WS
-colcon build
 
