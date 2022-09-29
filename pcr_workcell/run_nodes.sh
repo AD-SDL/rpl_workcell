215b58d94ad666f6f21d9ddf9107bc91ef1545d2
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-28 21:18:41 -0500

    fix run nodes

--- pcr_workcell/run_nodes.sh
+++ pcr_workcell/run_nodes.sh
@@ -4,31 +4,32 @@ session="nodes"
 tmux new-session -d -s $session
 
 window=0
-tmux new-window -t $session:$window -n 'pf400_module'
+tmux new-window -t $session:$window -n 'pf400'
+tmux rename-window -t $session:$window 'pf400'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 tmux send-keys -t $session:$window 'ros2 launch pf400_client pf400_client.launch.py' C-m
 
 window=1
-tmux new-window -t $session:$window -n 'sp_module'
+tmux new-window -t $session:$window -n 'sealerpeeler'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 tmux send-keys -t $session:$window 'ros2 launch sp_module_client sp_module.launch.py' C-m
 
 window=2
-tmux new-window -t $session:$window -n 'sciclops_module'
+tmux new-window -t $session:$window -n 'sciclops'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 tmux send-keys -t $session:$window 'ros2 launch sciclops_module_client sciclops_module.launch.py' C-m
 
 window=3
-tmux new-window -t $session:$window -n 'ot2_module'
+tmux new-window -t $session:$window -n 'ot2_alpha'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 tmux send-keys -t $session:$window 'export robot_ip=192.168.50.197' C-m #this need to become an arg on the roslaunch
 tmux send-keys -t $session:$window 'export robot_name=ot2_pcr_alpha' C-m #this need to become an arg on the roslaunch
 tmux send-keys -t $session:$window 'ros2 launch ot2_module_client ot2_module.launch.py' C-m
 
-# window=4
-# tmux new-window -t $session:$window -n 'biometra_module'
-#tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-# tmux send-keys -t $session:$window 'ros2 launch biometra_module_client biometra_module.launch.py' C-m
+window=4
+tmux new-window -t $session:$window -n 'biometra_module'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch biometra_module_client biometra_module.launch.py' C-m
 
 tmux attach-session -t $session
 
