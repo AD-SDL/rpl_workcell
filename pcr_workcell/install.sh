30c8635b0b05c3bc43034d90eb1a853b3140e3c1
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-28 20:09:57 -0500

    fix install

--- pcr_workcell/install.sh
+++ pcr_workcell/install.sh
@@ -10,12 +10,12 @@ git clone https://github.com/AD-SDL/hudson_module
 git clone https://github.com/AD-SDL/pf400_module
 git clone --recurse-submodules https://github.com/AD-SDl/ot2_module 
 git clone https://github.com/AD-SDL/wei_ros
+git clone https://github.com/AD-SDL/rpl_wei
 cd ..
 rosdep install --from-paths src --ignore-src --rosdistro humble -y
 colcon build
 
 
-git clone https://github.com/AD-SDL/rpl_wei
 cd $ROS_WS/src/rpl_wei
 pip3 install -r requirements/requirements.txt
 pip3 install -e .
