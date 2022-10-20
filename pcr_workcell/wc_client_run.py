7ff05b6dbe4de1f230c2cac75c35a49da33529e2
Rafael Vescovi <ravescovi@anl.gov>
2022-10-20 10:50:47 -0500

    Changes needed to get ot2 to work on parker, pls cleanup

--- pcr_workcell/wc_client_run.py
+++ pcr_workcell/wc_client_run.py
@@ -33,18 +33,15 @@ def wei_service_callback(step: Step, **kwargs):
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("-wc", "--workcell", help="Path to workcell file", type=Path)
     parser.add_argument("-wf", "--workflow", help="Path to workflow file", type=Path, required=True)
     parser.add_argument("-v", "--verbose", help="Extended printing options", action="store_true")
 
     return parser.parse_args()
 
 def main(args):
-    wc_file_path = args.workcell.resolve()
     wf_file_path = args.workflow.resolve()
     
     wei_client = WEI(
-        wc_file_path,
         wf_file_path,
         workcell_log_level=logging.DEBUG,
         workflow_log_level=logging.DEBUG,
