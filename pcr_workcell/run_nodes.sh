b7e86ef6b28dbe1f8f817457644f5864785d7cbd
Rafael Vescovi <ravescovi@anl.gov>
2022-10-26 15:38:44 -0500

    From Wednesday morning demo

--- pcr_workcell/run_nodes.sh
+++ pcr_workcell/run_nodes.sh
@@ -8,7 +8,7 @@ window=0
 tmux new-window -t $session:$window -n 'pf400'
 tmux rename-window -t $session:$window 'pf400'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-#tmux send-keys -t $session:$window 'ros2 launch pf400_client pf400_client.launch.py'  C-m
+tmux send-keys -t $session:$window 'ros2 launch pf400_client pf400_client.launch.py'  C-m
 
 window=1
 tmux new-window -t $session:$window -n 'sealerpeeler'
