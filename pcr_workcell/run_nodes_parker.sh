b9e1903cadb0e545b4887c1cd63fbde14ce50236
Dozgulbas <dozgulbas@anl.gov>
2023-02-08 12:19:27 -0600

    Updating sp module node run

--- pcr_workcell/run_nodes_parker.sh
+++ pcr_workcell/run_nodes_parker.sh
@@ -20,6 +20,10 @@ tmux set -g mouse on
 # tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 # tmux send-keys -t $session:$window 'ros2 launch sp_module_client peeler.launch.py' C-m
 
+window=1
+tmux new-window -t $session:$window -n 'sealerpeeler'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch sp_module_client sp_module_client.launch.py' C-m
 
 window=3
 tmux new-window -t $session:$window -n 'sciclops'
