b2c632a9aa3bb5c4e53e93216924556586495bb9
Dozgulbas <dozgulbas@anl.gov>
2023-02-08 14:21:07 -0600

    Update with camera module launch

--- pcr_workcell/run_nodes_strange.sh
+++ pcr_workcell/run_nodes_strange.sh
@@ -10,5 +10,10 @@ tmux rename-window -t $session:$window 'pf400'
 tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 tmux send-keys -t $session:$window 'ros2 launch pf400_client pf400_client.launch.py'  C-m
 
+window=1
+tmux new-window -t $session:$window -n 'camera_module'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch camera_module_client camera_publisher.launch.py camera_name:=camera_module camera_number:=1' C-m
+
 tmux attach-session -t $session
 
