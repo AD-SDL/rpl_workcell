18e9597cd86508cbdf093e6a57ef6d23ceebb287
Dozgulbas <dozgulbas@anl.gov>
2023-03-30 13:25:47 -0500

    Bug fix

--- bio_workcell/demo.py
+++ bio_workcell/demo.py
@@ -2,12 +2,13 @@
 
 import logging
 from pathlib import Path
+from argparse import ArgumentParser
 from rpl_wei.wei_workcell_base import WEI
 
 def main():
     wf_path = Path('/home/rpl/workspace/rpl_workcell/bio_workcell/workflows/platecrane_test.yaml')
 
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level= logging.ERROR, workflow_log_level=logging.ERROR)
+    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level=logging.ERROR, workflow_log_level=logging.ERROR)
 
     payload={
         'temp': 37.0, 
@@ -20,6 +21,7 @@ def main():
     run_info = wei_client.run_workflow(payload=payload)
     print(run_info)
     ## ADD PUBLISH HERE!!
+
 
 if __name__ == "__main__":
     main()
