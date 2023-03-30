e6a949028768b0aeb1cb99c8147662e3452c167d
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-03-30 11:25:27 -0500

    cleanup + biometra

---
+++ pcr_workcell/test/camera.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from rpl_wei.wei_workcell_base import WEI
+
+def main():
+    wf_path = Path('/home/rpl/workspace/rpl_workcell/pcr_workcell/workflows/test_camera.yaml')
+
+    wei_client = WEI(
+        wf_path.resolve()    )
+
+    wf_id = list(wei_client.get_workflows().keys())[0]
+
+    payload={}
+    wei_client.run_workflow(wf_id, payload=payload)
+
+if __name__ == "__main__":
+    main()
