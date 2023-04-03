13536dae73fc9cfc773f447b823a344b1b01be50
Dozgulbas <dozgulbas@anl.gov>
2023-04-03 13:07:38 -0500

    Updating the demo protocols and adding the ur5 workflow

--- pcr_workcell/demo.py
+++ pcr_workcell/demo.py
@@ -5,8 +5,7 @@ from pathlib import Path
 from rpl_wei.wei_workcell_base import WEI
 
 def main():
-    wf_path = Path('/home/rpl/workspace/rpl_workcell/bio_workcell/workflows/liconic_test.yaml')
-
+    wf_path = Path('/home/rpl/workspace/rpl_workcell/pcr_workcell/workflows/demo.yaml')
     wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level= logging.ERROR, workflow_log_level=logging.ERROR)
 
 
