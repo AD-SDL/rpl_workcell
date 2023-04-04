c261b5b01726eec9817a11b5339e6c2039a6e0e9
Dozgulbas <dozgulbas@anl.gov>
2023-04-04 14:10:30 -0500

    Fixing launch commands

--- scripts/run_bionuc.sh
+++ scripts/run_bionuc.sh
@@ -10,20 +10,20 @@ tmux set -g mouse on
 # tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
 # tmux send-keys -t $session:$window 'ros2 launch camera_module_client camera_publisher.launch.py camera_name:=camera_sp camera_number:=1' C-m
 
-# window=1
-# tmux new-window -t $session:$window -n 'sealer'
-# tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-# tmux send-keys -t $session:$window 'ros2 launch a4s_sealer_client a4s_sealer_client.launch.py sealer_port:=/dev/ttyUSB1' C-m
+window=1
+tmux new-window -t $session:$window -n 'sealer'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch a4s_sealer_client a4s_sealer_client.launch.py sealer_port:=/dev/ttyUSB1' C-m
 
-# window=2
-# tmux new-window -t $session:$window -n 'peeler'
-# tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-# tmux send-keys -t $session:$window 'ros2 launch brooks_peeler_client brooks_peeler_client.launch.py peeler_port:/dev/ttyUSB5' C-m
+window=2
+tmux new-window -t $session:$window -n 'peeler'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch brooks_peeler_client brooks_peeler_client.launch.py peeler_port:=/dev/ttyUSB5' C-m
 
-# window=3
-# tmux new-window -t $session:$window -n 'sciclops'
-# tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
-# tmux send-keys -t $session:$window 'ros2 launch sciclops_module_client sciclops_module.launch.py' C-m
+window=3
+tmux new-window -t $session:$window -n 'platecrane'
+tmux send-keys -t $session:$window 'source ~/wei_ws/install/setup.bash' C-m
+tmux send-keys -t $session:$window 'ros2 launch platecrane_client platecrane_client.launch.py ' C-m
 
 # window=4
 # tmux new-window -t $session:$window -n 'ot2_alpha'
