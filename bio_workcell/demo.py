bfb94626eb1d0184a59eda0c57a0074c73699eee
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-04-04 17:11:49 -0500

    add publish to demo

--- bio_workcell/demo.py
+++ bio_workcell/demo.py
@@ -4,6 +4,8 @@ import logging
 from pathlib import Path
 from argparse import ArgumentParser
 from rpl_wei.wei_workcell_base import WEI
+from .tools.publishv2 import publish_iter
+from pathlib import Path
 
 def main():
     wf_path = Path('/home/rpl/workspace/rpl_workcell/bio_workcell/workflows/demo.yaml')
@@ -20,8 +22,22 @@ def main():
 
     run_info = wei_client.run_workflow(payload=payload)
     print(run_info)
-    ## ADD PUBLISH HERE!!
 
+    exp_path =  Path(exp_path)
+    exp_label = Path(exp_label)
+    exp_folder = exp_path / exp_label
+    
+    # if not (os.path.isdir(exp_path)):
+    #     os.makedirs(exp_path)
+    #     print("makingdir")
+    # if not (os.path.isdir(exp_folder)):
+    #     os.mkdir(exp_folder)
+    # if not (os.path.isdir(exp_folder/"results")):
+    #     os.mkdir(exp_folder/"results") 
+
+    exp_folder = run_info['run_folder']
+    print("publishing:")
+    publish_iter(exp_folder/"results", exp_folder)
 
 if __name__ == "__main__":
     main()
