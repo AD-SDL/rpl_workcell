a7e612cd0d47899990713f82c178db90c83d967e
tginsbu1 <tsg44@cornell.edu>
2023-04-26 14:30:36 -0500

    changes to readme

--- color_picker/tools/test_publish_v2.py
+++ color_picker/tools/test_publish_v2.py
@@ -1,4 +1,4 @@
 from publish_v2 import publish_iter
 from pathlib import Path
-publish_iter(Path("/home/rpl/experiments/ColorPicker_41_120_131_2023-04-25/results"), Path("ColorPicker_41_120_131_2023-04-25"))
+publish_iter(Path("/home/rpl/experiments/ColorPicker_138_97_144_2023-04-25/results"), Path("ColorPicker_138_97_144_2023-04-25"))
 
