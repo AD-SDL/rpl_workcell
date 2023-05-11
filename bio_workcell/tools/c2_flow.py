829c3c39b4c759034758da541c1aa3c3ee0fe856
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-05-11 14:42:29 -0500

    c2_fixes

--- bio_workcell/tools/c2_flow.py
+++ bio_workcell/tools/c2_flow.py
@@ -10,7 +10,7 @@ from pathlib import Path
 class C2Flow(GladierBaseClient):
     globus_group = 'dda56f31-53d1-11ed-bd8b-0db7472df7d6'
     gladier_tools = [
-       # 'gladier_tools.transfer.Transfer',
+       'gladier_tools.globus.Transfer',
         C2_read_hidex,
         C2_check_contam,
         C2_blank_adjust,
@@ -22,10 +22,13 @@ class C2Flow(GladierBaseClient):
 def c2_flow(exp_name,plate_n,time, local_path, fname):
         flow_input = {
             'input': {
-                'source_globus_endpoint':'c819ce5c-d3e4-11ed-a9ce-63ca5f6c6821', #hudson ep
-                'destination_globus_endpoint':'f9726362-96a7-11ed-b310-55098fa75e99', #ripchip ep
-                'funcx_endpoint_compute':'95038e17-339b-4462-9c9f-a8473809af25', #ripchip funcx
-                'funcx_endpoint_non_compute':'95038e17-339b-4462-9c9f-a8473809af25', #ripchip funcx
+                'transfer_source_endpoint_id':'2f3968d6-d8a4-11ed-971f-e54704575ba', #hudson ep
+                'transfer_source_path': str(local_path / fname),
+                'transfer_destination_endpoint_id':'e69053b2-f02f-11ed-ba44-09d6a6f08166', #biopotts ep
+                'transfer_destination_path': '/home/rpl/test/',
+                'transfer_recursive': False,
+                'funcx_endpoint_compute':'b246dc22-4cc6-406f-bd44-3748b775f3bb', #biopotts funcx
+                'funcx_endpoint_non_compute':'b246dc22-4cc6-406f-bd44-3748b775f3bb', #biopotts funcx
                 'exp_name':exp_name,
                 'plate_n':plate_n,
                 'make_input': local_path,
@@ -33,7 +36,7 @@ def c2_flow(exp_name,plate_n,time, local_path, fname):
                 'remote_file': fname,
                 'csv_file': fname.split('.')[0] +".csv",
                 'csv_file_corr': fname +"_corr.csv",
-                'proc_folder': str(Path(local_path) ),
+                'proc_folder': str(local_path),
                 'time':time,
                 'publishv2': {
                     'dataset': local_path,
@@ -48,15 +51,6 @@ def c2_flow(exp_name,plate_n,time, local_path, fname):
                     'destination':str("/portal/bio"),
                     'visible_to' : ['public']
                    }
-                # 'pilot': {
-                #     'dataset': str(folder_path.expanduser()),
-                #     'index': '4e2884b0-e585-4913-8a33-4be155ebb06c',
-                #     'project': 'bio',
-                #     'source_globus_endpoint': '95038e17-339b-4462-9c9f-a8473809af25',
-                #     'source_collection_basepath': '/',
-                #     'metadata': {},
-                #     'destination':str(dest_path)
-                #    }
                 }
             }
 
@@ -71,7 +65,7 @@ def c2_flow(exp_name,plate_n,time, local_path, fname):
      
         
 if __name__ == "__main__":
-  local_path = "/home/tginsbu/workspace/rpl_workcell/bio_workcell/demo_data/example_report"
-  fname = "Campaign1_noIncubate2_20221201_160907.xlsx"
+  local_path = Path("/C/labautomation/data_wei/proc/")
+  fname = "Campaign2_wei_(10)_20230501_143510.xlsx"
 
   c2_flow("test_exp", 1, "time", local_path, fname)
