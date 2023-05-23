9c95c795a5db6f60039c2035f4827a97f9ef4eb8
tginsbu1 <tsg44@cornell.edu>
2023-05-23 11:19:55 -0500

    getting hidex ready

--- pcr_workcell/demo_ur.py
+++ pcr_workcell/demo_ur.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from rpl_wei.wei_workcell_base import WEI
 
 def main():
-    wf_path = Path('/home/rpl/workspace/rpl_workcell/pcr_workcell/workflows/ur5e_wf.yaml')
-
-    wei_client = WEI(wf_config = wf_path.resolve(), workcell_log_level= logging.ERROR, workflow_log_level=logging.ERROR)
+    wf_path = Path('/home/rpl/workspace/rpl_workcell/pcr_workcell/workflows/test_rest.yaml')
+    print(wf_path.resolve())
+    wei_client = WEI(wf_config = wf_path.resolve())#, workcell_log_level= logging.ERROR, workflow_log_level=logging.ERROR)
     payload={}
     run_info = wei_client.run_workflow(payload=payload)
 
