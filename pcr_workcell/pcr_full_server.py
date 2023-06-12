686697a5530da37e34ca3a8b6e3863ae52ce55aa
tginsbu1 <tsg44@cornell.edu>
2023-06-12 14:56:53 -0500

    string

--- pcr_workcell/pcr_full_server.py
+++ pcr_workcell/pcr_full_server.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-# from tools.publish import publish_iter
 from rpl_wei import Experiment
 
 def main():
     wf_path = Path('/home/rpl/workspace/rpl_workcell/pcr_workcell/workflows/qtest.yaml')
-    v = Experiment('127.0.0.1', '8000')
-    t = v.run_job(wf_path.resolve())
+    exp = Experiment('127.0.0.1', '8000', 'Full PCR Test')
+    flow_info = exp.run_job(wf_path.resolve())
 
-    print(t)
-    t = v.query_job(t['job_id'])
-    print(t)
-    payload={}
-    #run_info = wei_client.run_workflow(payload=payload)
-    # publish_iter(run_info["run_dir"], run_info["run_dir"])
+    print(flow_info)
+    flow_state = exp.query_job(flow_info['job_id'])
+    print(flow_state)
+
 if __name__ == "__main__":
     main()
