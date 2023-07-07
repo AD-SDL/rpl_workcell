dce1970de362917576f60f151200561bf95edcea
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-07-07 13:29:57 -0500

    cleanup format

--- color_picker/test_publish_v2.py
+++
@@ -1,4 +0,0 @@
-from tools.publish_v2 import publish_iter
-from pathlib import Path
-publish_iter(Path("/home/rpl/experiments/ColorPicker_209_205_11_2023-05-02/results"), Path("ColorPicker_209_205_11_2023-05-02"))
-
