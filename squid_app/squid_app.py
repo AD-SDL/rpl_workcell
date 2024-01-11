f6519a32694c44e312ce4011224efcc56a995ed4
Ryan Lewis <accounts+github@ryandlewis.dev>
2024-01-11 16:34:35 -0600

    Scaffold squid app

---
+++ squid_app/squid_app.py
@@ -0,0 +1,17 @@
+
+
+
+# Create Inks (N9)
+# Supply RPL Workcell (Inks from N9, plates from Storage?)
+# Move Plates to OT2
+# Run Color Picker:
+## TODO
+# Do Analysis
+# Repeat until failure
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
