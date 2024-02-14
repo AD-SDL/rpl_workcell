3aca7f2e3260694a40b1872941f62a425d5b497a
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2024-02-14 16:25:57 -0600

    update install parker

--- scripts/install_parker.sh
+++ scripts/install_parker.sh
@@ -7,7 +7,7 @@ cd $WS
 ##Peeler
 git clone https://github.com/AD-SDL/a4s_sealer_module
 cd a4s_sealer_module
-pip install -r requirements.txt
+git pull
 pip install . 
 cd $WS
 
@@ -15,33 +15,33 @@ cd $WS
 ##Sealer
 git clone https://github.com/AD-SDL/brooks_xpeel_module
 cd brooks_xpeel_module
-pip install -r requirements.txt
+git pull
 pip install . 
 cd $WS
 
 ##Sciclops
 git clone https://github.com/AD-SDL/hudson_platecrane_module
 cd hudson_platecrane_module
-pip install -r requirements.txt
+git pull
 pip install . 
 cd $WS
 
 ##OT2
 git clone https://github.com/AD-SDl/ot2_module
 cd ot2_module
-pip install -r requirements.txt
+git pull
 pip install . 
 cd $WS
 
 ##PF400
 git clone https://github.com/AD-SDL/pf400_module
 cd pf400_module
-pip install -r requirements.txt
+git pull
 pip install . 
 cd $WS
 
 ## Camera Module
 git clone https://github.com/AD-SDL/camera_module.git
 cd camera_module
-pip install -r requirements.txt
+git pull
 cd $WS
