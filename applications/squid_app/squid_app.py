bc830840d1f7f14778e9f799689705907f083342
Ryan Lewis <accounts+github@ryandlewis.dev>
2024-03-21 16:42:21 -0500

    Cleanup, experimenting with experiments

--- applications/squid_app/squid_app.py
+++ applications/squid_app/squid_app.py
@@ -37,45 +37,45 @@ def main():
         help="Port of Workcell",
     )
     parser.add_argument(
-        "--experiment_id", type=str, help="ID of the experiment", default=""
+        "--experiment_id", type=str, help="ID of the experiment", default=None
     )
     args = parser.parse_args()
     experiment = wei.ExperimentClient(
         server_addr=args.rpl_wc_server,
         server_port=args.rpl_wc_port,
-        experiment_name="Squid Benchmark Experiment",
-        # experiment_id=args.experiment_id,
+        experiment_design="./squid_app.design.yaml",
+        experiment_id=args.experiment_id,
     )
 
     wf_dir = Path(__file__).parent.absolute() / "workflows"
-    protocol_dir = Path(__file__).parent.absolute() / "protocols"
+    # protocol_dir = Path(__file__).parent.absolute() / "protocols"
 
-    experiment.start_run(
-        workflow_file=wf_dir / "cp_wf_reset_colors.yaml",
-        payload={},
-        blocking=True,
-        simulate=False,
-    )
+    # experiment.start_run(
+    #     workflow_file=wf_dir / "cp_wf_reset_colors.yaml",
+    #     payload={},
+    #     blocking=True,
+    #     simulate=False,
+    # )
     experiment.start_run(
         workflow_file=wf_dir / "cp_wf_newplate.yaml",
         payload={},
         blocking=True,
         simulate=False,
     )
-    experiment.start_run(
-        workflow_file=wf_dir / "cp_wf_mixcolor.yaml",
-        payload={
-            "use_existing_resources": False,
-            "color_A_volumes": 10.0,
-            "color_B_volumes": 10.0,
-            "color_C_volumes": 10.0,
-            "color_D_volumes": 10.0,
-            "destination_wells": ["A1"],
-            "config_path": str(protocol_dir / "combined_protocol.yaml"),
-        },
-        blocking=True,
-        simulate=False,
-    )
+    # experiment.start_run(
+    #     workflow_file=wf_dir / "cp_wf_mixcolor.yaml",
+    #     payload={
+    #         "use_existing_resources": False,
+    #         "color_A_volumes": 10.0,
+    #         "color_B_volumes": 10.0,
+    #         "color_C_volumes": 10.0,
+    #         "color_D_volumes": 10.0,
+    #         "destination_wells": ["A1"],
+    #         "config_path": str(protocol_dir / "combined_protocol.yaml"),
+    #     },
+    #     blocking=True,
+    #     simulate=False,
+    # )
 
 
 if __name__ == "__main__":
