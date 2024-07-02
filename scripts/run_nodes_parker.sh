ec9602b82e53d92a683fc4c0ef887cdbe572018a
Dozgulbas <dozgulbas@anl.gov>
2024-07-02 09:26:10 -0500

    Make parker script less brittle

--- scripts/run_nodes_parker.sh
+++ scripts/run_nodes_parker.sh
@@ -3,7 +3,9 @@
 ssh rpl@parker.cels.anl.gov
 
 cd ~/workspace
-git clone https://github.com/ad-sdl/rpl_workcell
+if [ ! -d rpl_workcell ]; then
+	git clone https://github.com/ad-sdl/rpl_workcell
+fi
 cd rpl_workcell/docker
 docker compose -f parker-nodes.compose.yaml up -d
 
